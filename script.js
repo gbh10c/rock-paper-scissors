2f5adcc65b102b5d9ead2ec7e7b12c547cb478e8
gbh10c <grandcasa10@gmail.com>
2021-11-05 09:48:08 -0400

    Plays 5 games, but needs to keep score and calculate ties properly.

--- script.js
+++ script.js
@@ -3,20 +3,33 @@ function computerPlay() {
     let computerHand = computerOption[Math.floor(Math.random() * 3)];
     return computerHand;
 }
+
 function userPlay() {
     let input = prompt("Type 'rock', 'paper', or 'scissors'.");
     let answer = input.toLowerCase();
+        if (answer != 'rock' && answer != 'paper' && answer != 'scissors') {
+            return prompt("Please enter a valid selection: 'rock', 'paper', or 'scissors'!");
+        } else {
     return answer;
+        }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
     playerSelection = userPlay();
     computerSelection = computerPlay();
     if (playerSelection === computerSelection) {
-        console.log("It's a tie!");
+        return "It's a tie!";
+    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        return "You win, " + playerSelection + " beats " + computerSelection + "!";
     } else {
-        console.log(playerSelection, computerSelection);
+        return "You lose, " + computerSelection + " beats " + playerSelection + ".";
     }
 }
 
-//playRound();
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound());
+    }
+}
+
+game();
