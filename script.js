a69d7f874511ead8c5df83cf314f363bf058da4b
gbh10c <grandcasa10@gmail.com>
2021-11-04 20:05:29 -0400

    Working on making user entry lower case

---
+++ script.js
@@ -0,0 +1,14 @@
+function computerPlay() {
+    let computerOption = ['rock', 'paper', 'scissors'];      
+    let computerHand = computerOption[Math.floor(Math.random() * 3)];
+    return computerHand;
+}
+
+    let input = prompt("Type 'rock', 'paper', or 'scissors'.");
+    let answer = input.toLowerCase();
+    console.log(answer);
+
+function playRound(playerSelection, computerSelection) {
+    
+}
+
