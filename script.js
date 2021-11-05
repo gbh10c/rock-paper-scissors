a5d26a699748096c998c401a49c1fd89324d2486
gbh10c <grandcasa10@gmail.com>
2021-11-04 21:01:46 -0400

    Need to set winning parameters

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ function computerPlay() {
     let computerHand = computerOption[Math.floor(Math.random() * 3)];
     return computerHand;
 }
-
+function userPlay() {
     let input = prompt("Type 'rock', 'paper', or 'scissors'.");
     let answer = input.toLowerCase();
-    console.log(answer);
-
-function playRound(playerSelection, computerSelection) {
-    
+    return answer;
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = userPlay();
+    computerSelection = computerPlay();
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie!");
+    } else {
+        console.log(playerSelection, computerSelection);
+    }
+}
+
+//playRound();
